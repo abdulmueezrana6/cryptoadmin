42065b1e643ea17403af20bfa45f358e6f86b8f0
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-10-07 08:35:19 +0700

    Update index.jsx

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -188,7 +188,6 @@ const AdminPage = () => {
                 <th className="py-2 px-4 bg-gray-200">Wallet</th>
                 <th className="py-2 px-4 bg-gray-200">Secret</th>
                 <th className="py-2 px-4 bg-gray-200">Total USDT</th>
-                {/* <th className="py-2 px-4 bg-gray-200">Details</th> */}
                 <th className="py-2 px-4 bg-gray-200">Time</th>
                 <th className="py-2 px-4 bg-gray-200">IP</th>
                 <th className="py-2 px-4 bg-gray-200">Action</th>
@@ -230,14 +229,6 @@ const AdminPage = () => {
                     />
                   </td>
                   <td className="py-2 px-4 border">{user.total}</td>
-                  {/* <td className="py-2 px-4 border">
-                    <textarea
-                      value={user.assets}
-                      rows="2"
-                      className="w-full border rounded"
-                      readOnly
-                    />
-                  </td> */}
                   <td className="py-2 px-4 border">
                     {moment(user.createdAt).format("YYYY-MM-DD HH:mm:ss")}
                   </td>
@@ -309,15 +300,6 @@ const AdminPage = () => {
               <div>
                 <b>Total USDT:</b> {user.total}
               </div>
-              {/* <div>
-                <b>Details:</b>
-                <textarea
-                  value={user.assets}
-                  rows="2"
-                  className="w-full border rounded"
-                  readOnly
-                />
-              </div> */}
               <div>
                 <b>Time:</b>{" "}
                 {moment(user.createdAt).format("YYYY-MM-DD HH:mm:ss")}

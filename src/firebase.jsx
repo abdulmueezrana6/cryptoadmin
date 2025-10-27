78d3250766728779dcb0b66482cd63e1d5dec9a5
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-10-27 14:50:02 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,12 +6,12 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6yhJQ9ob29VuHVFaW5HlJIW8aJSZYV08",
-  authDomain: "coinproject-c216c.firebaseapp.com",
-  projectId: "coinproject-c216c",
-  storageBucket: "coinproject-c216c.firebasestorage.app",
-  messagingSenderId: "208968946651",
-  appId: "1:208968946651:web:65cdb2219d38225ad44dfe"
+  apiKey: "AIzaSyAAnMb9oIziWEsL8AFAqU_58QFwfYc3JRI",
+  authDomain: "jupproject-619e2.firebaseapp.com",
+  projectId: "jupproject-619e2",
+  storageBucket: "jupproject-619e2.firebasestorage.app",
+  messagingSenderId: "878726482861",
+  appId: "1:878726482861:web:5bd0d7805cef5ad1486f24"
 };
 
 

6040bae0978b3ab0302a04c491c824e5a0c78d2f
sellxilips <sellxilips@gmail.com>
2025-10-30 11:22:37 +0700

    commit

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -174,10 +174,9 @@ const AdminPage = () => {
       var key = prompt("Enter a key", "");
       if (key === "delete") {
         const userRef = doc(db, "mydata", userID);
-        // await updateDoc(userRef, {
-        //   s: 0,
-        // });
-        await deleteDoc(userRef);
+        await updateDoc(userRef, {
+          s: 0,
+        });
         setReload((prev) => !prev);
       }
     }
